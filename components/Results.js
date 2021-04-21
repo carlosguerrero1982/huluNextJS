18a0ef4d5d5fab9eabc34cde52d56538144278af
carlosguerrero1982 <carlos.guerrero.rodriguez.dev@gmail.com>
2021-04-21 14:12:40 +0200

    first commit

---
+++ components/Results.js
@@ -0,0 +1,17 @@
+import Thumbnail from './Thumbnail'
+
+function Results({results}) {
+
+    
+    return (
+        <div>
+            {
+               results.results.map((result)=>(
+                   <Thumbnail key={result.id} result={result} />
+               ))
+            }
+        </div>
+    )
+}
+
+export default Results
